04ac442f58f20ab77f031dc31fc6bb166e71fa1b
Blake Niemyjski <bniemyjski@gmail.com>
2018-09-11 13:12:16 -0500

    reverted back to 1.x so you can use ms caching
    https://github.com/aspnet/Caching/issues/410

--- test/Foundatio.Redis.Tests/SharedConnection.cs
+++ test/Foundatio.Redis.Tests/SharedConnection.cs
@@ -11,8 +11,10 @@ namespace Foundatio.Redis.Tests {
             if (String.IsNullOrEmpty(connectionString))
                 return null;
 
-            if (_muxer == null)
+            if (_muxer == null) {
                 _muxer = ConnectionMultiplexer.Connect(connectionString);
+                _muxer.PreserveAsyncOrder = false;
+            }
 
             return _muxer;
         }
